87d27946d7c1194e112f5efb84741a22cc96a004
zhangbo <xiaobo21@163.com>
2020-12-04 18:10:35 +0800

    test: prettier -fix

--- src/pages/tabbar/index/components/Space.vue
+++ src/pages/tabbar/index/components/Space.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="space"></div>
+  <div class="space" />
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+  import { Vue, Component } from 'vue-property-decorator';
 
-@Component
-export default class Space extends Vue {
-  name = "Space";
-}
+  @Component
+  export default class Space extends Vue {
+    name = 'Space';
+  }
 </script>
 
 <style lang="scss" scoped></style>
