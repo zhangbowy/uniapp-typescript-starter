bb0dda006662e8f59d672ddb015ac7fbeb14d746
zhangbo <xiaobo21@163.com>
2020-12-05 09:46:38 +0800

    test: 1

--- src/pages/tabbar/index/components/RichText.vue
+++ src/pages/tabbar/index/components/RichText.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="rich-text">
+  <view class="rich-text" class="1">
     <u-parse
       :html="data"
       :selectable="true"
@@ -13,40 +13,41 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+  import { Vue, Component, Prop } from 'vue-property-decorator';
 
-@Component
-export default class RichText extends Vue {
-  // 富文本
-  name = "RichText";
-  @Prop({
-    default: ``,
-    type: String,
-  })
-  data!: String;
+  @Component
+  export default class RichText extends Vue {
+    // 富文本
+    name = 'RichText';
+    @Prop({
+      default: ``,
+      type: String,
+    })
+    data!: String;
 
-  style: Record<string, any> = {
-    // 字符串的形式
-    p:
-      "color: #999999;font-size:32rpx;margin:4px 0;width: 100%;box-sizing:border-box;padding: 6px;",
-    span: "font-size: 30rpx;",
-    img: "border-radius: 4px;",
-  };
-}
+    style: Record<string, any> = {
+      // 字符串的形式
+      p:
+        'color: #999999;font-size:32rpx;margin:4px 0;width: 100%;box-sizing:border-box;padding: 6px;',
+      span: 'font-size: 30rpx;',
+      img: 'border-radius: 4px;',
+    };
+  }
 </script>
 
 <style lang="scss" scoped>
-.rich-text {
-  padding: 10px;
-  overflow: hidden;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-sizing: border-box;
-}
-.u-content {
-  padding: 10px;
-  width: 100%;
-}
+  .rich-text {
+    display: flex;
+    width: 100%;
+    padding: 10px;
+    overflow: hidden;
+    box-sizing: border-box;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .u-content {
+    width: 100%;
+    padding: 10px;
+  }
 </style>
